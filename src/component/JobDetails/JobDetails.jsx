092573e151916b19f796c05d3068187ee2d1f947
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-11 03:28:49 +0600

    ad to react tostify

--- src/component/JobDetails/JobDetails.jsx
+++ src/component/JobDetails/JobDetails.jsx
@@ -1,14 +1,38 @@
 import { useLoaderData, useParams } from 'react-router-dom';
 import './jobDetails.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faEnvelopeOpen, faLocationDot, faPhone, faSackDollar } from '@fortawesome/free-solid-svg-icons';
+import { addToDb, getShoppingCart } from '../utilities/fakedb';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const JobDetails = () => {
     const loadId = useParams();
     const loadData = useLoaderData();
     const getJob = loadData.filter(job => job.id === +loadId.jobId);
 
-    const { contactInformation, educationalRequirements, experiences, jobDescription, jobResponsibility, location, jobTitle, salary } = getJob[0];
+    const { id, contactInformation, educationalRequirements, experiences, jobDescription, jobResponsibility, location, jobTitle, salary } = getJob[0];
+
+    const handleLoadToAppliedJob = (id) => {
+        const loadAppliedJob = getShoppingCart()
+        for (const appliedId in loadAppliedJob) {
+            if (appliedId == id) {
+                toast.warn(' All ready applied', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+                return
+            }
+            else {
+                addToDb(id)
+            }
+        }
+        addToDb(id)
+    }
 
     return (
         <div>
@@ -49,9 +73,10 @@ const JobDetails = () => {
                         <img src="/public/Frame-4.png" alt="" />
                         <p><span>Address : </span> {location}</p>
                     </div>
-                    <button className='apply-now-btn'>Apply Now</button>
+                    <button onClick={() => handleLoadToAppliedJob(id)} className='apply-now-btn'>Apply Now</button>
                 </div>
             </div>
+            <ToastContainer />
         </div>
     );
 };

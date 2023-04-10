5850144754da89924ca59d0ec994fa2c346010bb
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-10 16:01:19 +0600

    ad to featired job component

---
+++ src/component/FeaturedJob/FeaturedJob.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './FeaturedJob.css';
+
+const FeaturedJob = () => {
+    return (
+        <div>
+            <div className='featured-info'>
+                <h2 className='featured-title'>Featured Jobs</h2>
+                <p className='featured-description'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedJob;
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import JobCategory from '../JobCategory/JobCategory';
+import FeaturedJob from '../FeaturedJob/FeaturedJob';
+import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
+    const jobs = useLoaderData();
+    console.log(jobs)
     return (
         <div>
             <Banner></Banner>
             <JobCategory></JobCategory>
+            <FeaturedJob></FeaturedJob>
         </div>
     );
 };

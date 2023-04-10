c3e306521640cc022fe0d709377ed905800ca11a
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-10 19:00:44 +0600

    display to job cart

--- src/component/Job/Job.jsx
+++ src/component/Job/Job.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import './Job.css';
 
 const Job = ({ job }) => {
-    console.log(job)
+    const { companyLogo, companyName, jobTitle, fullTimeOrPartTime, remoteOrOnsite, location, salary } = job;
     return (
-        <div>
-
+        <div className='job-container'>
+            <img src={companyLogo} alt="" />
+            <h2>{jobTitle}</h2>
+            <p>{companyName}</p>
+            <div className='job-time-container'>
+                <p>{fullTimeOrPartTime}</p>
+                <p>{remoteOrOnsite}</p>
+            </div>
+            <span>{location}</span>
+            <span>{salary}</span>
+            <br />
+            <button className='view-details-btn'>View Details</button>
         </div>
     );
 };

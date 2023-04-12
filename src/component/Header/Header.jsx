8071cc53a56ec4c250a959061e13812c5b14ffd4
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-12 12:09:42 +0600

    add to active link part

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import './Header.css';
-
+import ActiveLink from '../ActiveLink/ActiveLink';
 const Header = () => {
     return (
         <div >
             <div className='header-container'>
                 <h2 className='header-logo'>Job hiring</h2>
                 <nav className='nav-link'>
-                    <Link to="/">Home</Link>
-                    <Link to="/statistics">Statistics</Link>
-                    <Link to="/applied">Applied Jobs</Link>
-                    <Link to="/blog">Blog</Link>
+                    <ActiveLink to="/">Home</ActiveLink>
+                    <ActiveLink to="/statistics">Statistics</ActiveLink>
+                    <ActiveLink to="/applied">Applied Jobs</ActiveLink>
+                    <ActiveLink to="/blog">Blog</ActiveLink>
                 </nav>
                 <button className='header-btn'>Star Applying</button>
             </div>
